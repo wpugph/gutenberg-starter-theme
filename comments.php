78c0b3b77105dbc78ff68b33f8da6d53d55a8a3b
Erwin Pagulong <windongdev@gmail.com>
2019-05-17 22:51:08 +0800

    Added Custom Walker Comments

--- comments.php
+++ comments.php
@@ -50,8 +50,10 @@ if ( post_password_required() ) {
 		<ol class="comment-list">
 			<?php
 				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
+					'walker'				=> new ThemeWPUGPH_Comments(),
+					'style'      		=> 'ol',
+					'short_ping' 		=> true,
+					'avatar_size'		=> 50,
 				) );
 			?>
 		</ol><!-- .comment-list -->
