ae4e7130ef90ea5686602ed8273c087ecb75beb0
Erwin Pagulong <windongdev@gmail.com>
2019-05-24 16:26:31 +0800

    Added Custom Quote blocks

---
+++ inc/custom-blocks.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Custom blocks
+ *
+ * @package  TeamWPUGPHTheme
+ */
+
+/**
+ * Table block filter
+ */
+function teamwpugph_table_block_fitler( $block_content, $block ) {
+
+	if ( 'core/table' !== $block['blockName'] ) {
+		return $block_content;
+	}
+
+	$doc = new DOMDocument();
+	$doc->loadHTML( $block_content );
+	$table = $doc->getElementsByTagName( 'table' );
+	$table[0]->setAttribute( 'class', 'uk-table' );
+
+	$output = $doc->saveHTML();
+
+	return $output;
+}
+add_filter( 'render_block', 'teamwpugph_table_block_fitler', 10, 3 );
+
+/**
+ * Quote block filter
+ */
+function teamwpugph_quote_block_fitler( $block_content, $block ) {
+
+	if ( 'core/quote' !== $block['blockName'] ) {
+		return $block_content;
+	}
+
+	$html = new DOMDocument();
+	$html->loadHTML( $block_content );
+	$p = $html->getElementsByTagName( 'p' );
+	$cite = $html->getElementsByTagName( 'cite' );
+
+	$output = '<blockquote>';
+	$output .= '<p class="uk-margin-small-bottom">';
+	$output .= $p[0]->nodeValue;
+	$output .= '</p>';
+	$output .= '<footer>';
+	$output .= '<cite>';
+	$output .= $cite[0]->nodeValue;
+	$output .= '</cite>';
+	$output .= '</footer>';
+	$output .= '</blockquote>';
+
+	return $output;
+
+}
+add_filter( 'render_block', 'teamwpugph_quote_block_fitler', 10, 3 );
